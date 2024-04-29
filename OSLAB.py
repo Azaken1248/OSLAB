10998e1d1c5c38ad83e900e861b5b0f70ee3f9cb
Azaken1248 <suvkrm@gmail.com>
2024-04-29 20:16:19 +0530

    Adding Another Directory

--- OSLAB.py
+++ OSLAB.py
@@ -1,7 +1,19 @@
 import os
 
-directory = "Templates"
-fileList = os.listdir(directory)
+
+def chooseDir():
+    print("Select Parent Directory")
+    directory = "Templates"
+    dirList = os.listdir(directory)
+    
+    counter = 1
+    for dir in dirList:
+        print(str(counter)+".",dir,end = "\t")
+        counter += 1
+    
+    choice = int(input("\nEnter Your Choice: "))
+    
+    return list(os.listdir("Templates/"+dirList[choice-1])),dirList[choice-1]
 
 def menu(files):
     counter = 1
@@ -18,21 +30,22 @@ def menu(files):
     
     return choice,len(files)
 
-def getFile(path):
-    
+def getFile(path, dir):
     try:
-        buff = ""
-        with open("Templates/"+path,"r") as inputFile:
+        with open("Templates/"+dir+"/"+path, "r") as inputFile:
             buff = inputFile.read()
-        with open("Outputs/"+path, "w+") as outputFile:
+        with open("Outputs/"+dir+"/"+path, "w+") as outputFile:
             outputFile.write(buff)
-    except:
-        print("FileNotFound!!!")
-    
-    print("File Generated!Thank You!")
+        print("File Generated! Thank You!")
+    except FileNotFoundError as e:
+        print("FileNotFound:", e)
+    except Exception as e:
+        print("An error occurred:", e)
+
     
 
 while True:
+    fileList,dir = chooseDir()
     choice,n = menu(fileList)
     
     if(choice == -1):
@@ -41,4 +54,6 @@ while True:
     elif choice not in list(range(1,n+1)):
         print("INVALID CHOICE!")
     else:
-        getFile(fileList[choice-1])    
+        getFile(fileList[choice-1],dir)
+    
+        
